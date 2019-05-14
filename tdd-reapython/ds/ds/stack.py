13b2841af9fb380b18902b9effce75da98b4b89e
fermin <fermin.hdez@gmail.com>
2019-05-14 13:50:39 +0200

    update example tdd stack

--- tdd-reapython/ds/ds/stack.py
+++ tdd-reapython/ds/ds/stack.py
@@ -5,3 +5,12 @@ class Stack:
     def __len__(self):
         return len(self._storage)
 
+    def push(self, item):
+        self._storage.append(item)
+    
+    def pop(self):
+        try:
+            item = self._storage.pop()
+        except IndexError:
+            item = None
+        return item
--- tdd-reapython/ds/test/test_stack.py
+++ tdd-reapython/ds/test/test_stack.py
@@ -1,7 +1,26 @@
 from ds.stack import Stack
+import pytest
 
 
-def test_constructor():
-    s = Stack()
-    assert isinstance(s, Stack)
-    assert len(s) == 0
+@pytest.fixture
+def stack():
+    return Stack()
+
+
+def test_constructor(stack):
+    assert isinstance(stack, Stack)
+    assert len(stack) == 0
+
+# addd stack, call fixture stack()
+def test_push(stack):
+    stack.push(3)
+    assert len(stack) == 1
+    stack.push(5)
+    assert len(stack) == 2
+
+def test_pop(stack):
+    stack.push('wop')
+    stack.push('pow')
+    assert stack.pop() == 'pow'
+    assert stack.pop() == 'wop'
+    assert stack.pop() is None
